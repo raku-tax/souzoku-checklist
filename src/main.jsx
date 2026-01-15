2cff979bb2fb04845c4d81d4805d6543e1e79f3c
raku-tax <rakusou+github@raku-tax.co.jp>
2026-01-15 15:11:46 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Admin from './Admin.jsx'
 import './index.css'
+
+const path = window.location.pathname
+
+function Root() {
+  if (path === '/admin' || path === '/admin/') {
+    return <Admin />
+  }
+  return <App />
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 )
